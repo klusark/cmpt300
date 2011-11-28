3081190c4129a9c4546231edf9a518d167ae0e67
= <joel@teichroeb.net>
2011-11-27 23:46:39 -0800

    Cleanup the SSTF and Elevator comments

--- SSTF.cpp
+++ SSTF.cpp
@@ -1,3 +1,14 @@
+/*
+ * SSTF.cpp
+ *
+ * Created by Joel Teichroeb on 11/25/11
+ * for CMPT 300 Project 2.
+ *
+ * All rights reserved.
+ *
+ * The SSTF class as defined in SSTF.h.
+ *
+ */
 #include "SSTF.h"
 #include "HDMonitor.h"
 
@@ -7,3 +18,4 @@
 bool SSTF::operator< (const request &r) const{
 	return dist(H->currentTrack, track) < dist(H->currentTrack, r.track);
 }
+
